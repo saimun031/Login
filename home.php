b9987ad80c2f80e00debd66d1e7a9be2d247fb38
Mushfiquer <saimunrahman29@gmail.com>
2021-01-09 01:12:58 +0600

    Login

---
+++ home.php
@@ -0,0 +1,68 @@
+<?php include ('login.php')
+?>
+<html>
+<head>
+    <title> Welcome home</title>
+    <link rel="stylesheet" href="styleforhome.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+    <div class="saimun">
+        <div class="manu">
+            <ul>
+                <li class="logo"><img src="1266.jpg"></li>
+            <li>Home</li>
+            <li>About_Us</li>
+            <li>Contact</li>
+				<p style="color:white"> <a href="login.php">SignIn</a></p>
+			
+               <li><a href="login.php" class="sign">Sign In</a></li> 
+                        
+            </ul>
+        </div>
+        <div class="banner">
+        <div class="text">
+            <h1>That person who take care the old people or disable person own their house.<br>And who come home to take care the old people or disable person.<br> Or Everyone.
+            </h1>
+            <p>
+            That person information. Patient families information.<br>Our group is one of the topest and famous group in our country.
+            </p>
+            <h2>
+            If you are not regestared.<br>Click Here To Regester.
+            </h2> 
+            <p>
+             Otherwise You can't SIGNEIN.
+            </p>
+            <div class="reg">   
+                <br><li><a href="register.php" class="reg1"> Regestr Now</a></li>
+            </div>
+            <div class="pb">
+            <br><div class="pbin"><i class="fa fa-info-circle"></i>
+            </div>
+                <small><b> CLICK HERE TO KNOW ABOUT US.</b></small>
+            </div>
+            </div>
+            <div class="p2">
+            <img src="1273.jpg">
+            </div>
+        </div>
+        <div class="links">
+        <ul>
+           <li><i class="fa fa-share-alt"></i><p> SHERE TO</p></li> 
+            <li><i class="fa fa-audio-description"></i><p> RECENT NEWS</p></li> 
+            <li><i class="fa fa-btc"></i><p>CURENT FOUNDING</p></li> 
+            </ul>
+        </div>
+        <div class="sicon">
+            <ul>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>
+     </div>     
+    
+    
+    
+</body>
+</html>
